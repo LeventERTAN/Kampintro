6b20a3ffa3798008e5feaedcd183a47306f35a76
Levent <Levent@ETO60.ETO>
2021-01-16 23:44:23 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2=65;
+            //sayi1?? 30
+
+            //int, decimal, float, double, bool = değer tip
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] 999
+
+            //array, class,interface = referans tip
+
+            //bellekte stack ve heap iki alan var  stack değertip, heap referanstip
+
+            //stack sayi1=10    sayi2=30  değertiplerde sadece değer aktarılır ve sayi2 ile irtibat kopar
+
+            //stack            heap
+            //101/sayilar1      101/[10,20,30]
+            //102/sayilar2      102/[100,200,300]
+
+            // bu duruma pointer - gösterge denir
+
+            //Değişken türleri:  "int", "long", "float", "double", "decimal", "char", "bool", "byte", "short", "struct", "enum"
+            //Referans türleri:  "string", "object", "class", "interface", "array", "delegate", "pointer"
+        }
+    }
+}
---
+++ ETicaretUrunClassOdevi/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ETicaretUrunClassOdevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun Urun1 = new Urun();
+            Urun1.Fotograf = "1. Ürün Foto";
+            Urun1.UrunAciklama = "ASUS X515JF Notebook";
+            Urun1.Fiyat = 7269;
+            Urun1.IndirimOrani = "%10";
+            Urun1.IndirimliFiyat = 6179;
+
+            Urun Urun2 = new Urun();
+            Urun2.Fotograf = "2. Ürün Foto";
+            Urun2.UrunAciklama = "Huawei Matebook D 14 Notebook";
+            Urun2.Fiyat = 6998;
+            Urun2.IndirimOrani = "%15";
+            Urun2.IndirimliFiyat = 6299;
+
+            Urun Urun3 = new Urun();
+            Urun3.Fotograf = "3. Ürün Foto";
+            Urun3.UrunAciklama = "ASUS F543NA Notebook";
+            Urun3.Fiyat = 4351;
+            Urun3.IndirimOrani = "%15";
+            Urun3.IndirimliFiyat = 3699;
+
+            Urun[] Urunler = new Urun[] { Urun1, Urun2, Urun3 };
+
+            foreach (var Urun in Urunler)
+            {
+                Console.WriteLine(Urun.Fotograf);
+                Console.WriteLine(Urun.UrunAciklama);
+                Console.WriteLine(Urun.Fiyat);
+                Console.WriteLine(Urun.IndirimOrani);
+                Console.WriteLine(Urun.IndirimliFiyat);
+            }
+
+        }
+    }
+         class Urun
+        {
+            public string Fotograf { get; set; }
+            public string UrunAciklama { get; set; }
+            public float Fiyat { get; set; }
+            public string IndirimOrani { get; set; }
+            public float  IndirimliFiyat { get; set; }
+        }
+
+
+
+        
+    
+}
+
