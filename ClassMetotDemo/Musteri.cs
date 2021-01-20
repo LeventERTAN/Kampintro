9b6f21871724923da7b46f94af1519e958efb80c
Levent ERTAN <leventertan@gmail.com>
2021-01-20 23:50:15 +0300

    20 Ocak Ders Sonu

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int MusteriId { get; set; }
+        public int MusteriAd { get; set; }
+        public int MusteriSoyad { get; set; }
+        public int TCKimlikNo { get; set; }
+        public int MusteriTelefon { get; set; }
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public Musteri AddMusteri(Musteri musteri)
+        {
+            Console.Write("Müşteri Id: ");
+            musteri.MusteriId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Müşteri Adı ve Soyadı: ");
+            musteri.MusteriAd = Console.ReadLine();
+            Console.Write("Para Miktari: ");
+            musteri.Balance =
+        }
+
+
+    }
+}
